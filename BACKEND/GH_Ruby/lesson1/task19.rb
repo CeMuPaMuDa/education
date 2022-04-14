266bc12a091599b098bed165da4ee73d7008dc8b
CeMuPaMuDa <bepyc9i@gmail.com>
2022-04-14 22:36:10 +0300

    task 17-20

---
+++ BACKEND/GH_Ruby/lesson1/task19.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+puts '19. Дан целочисленный массив. Найти максимальный из его локальных максимумов.'
+puts "Исходный массив: #{arr_t18 = Array.new(15) { rand(-50..50) }}"
+arr = []
+result = arr_t18.each_cons(3) { |x, y, z| arr<<y if x < y && y > z  }
+
+puts "Результат: #{arr.max}"
