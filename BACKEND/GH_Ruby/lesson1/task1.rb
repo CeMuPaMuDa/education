78a49d22b0df50280f703046cfe369838a0eca7a
CeMuPaMuDa <bepyc9i@gmail.com>
2022-04-14 18:28:35 +0300

    fix task 1,2

--- BACKEND/GH_Ruby/lesson1/task1.rb
+++ BACKEND/GH_Ruby/lesson1/task1.rb
@@ -3,9 +3,6 @@
 puts '1. Дан целочисленный массив. Необходимо вывести вначале его элементы с четными индексами, а затем - с нечетными.'
 puts "Исходный массив: #{arr_t1 = Array.new(15) { rand(-50..50) }}"
 
-result = arr_t1.select
-               .with_index { |_, i| i.even? } +
-         arr_t1.reject
-               .with_index { |_, i| i.even? }
+result = arr_t1.partition.with_index { |_, i| i.even? }.flatten
 
 puts "Результат: #{result}"
--- BACKEND/GH_Ruby/lesson1/task2.rb
+++ BACKEND/GH_Ruby/lesson1/task2.rb
@@ -3,9 +3,6 @@
 puts '2. Дан целочисленный массив. Необходимо вывести вначале его элементы с нечетными индексами, а затем - четными.'
 puts "Исходный массив: #{arr_t2 = Array.new(15) { rand(-50..50) }}"
 
-result = arr_t2.reject
-               .with_index { |_, i| i.even? } +
-         arr_t2.select
-               .with_index { |_, i| i.even? }
+result = arr_t2.partition.with_index { |_, i| i.odd? }.flatten
 
 puts "Результат: #{result}"
