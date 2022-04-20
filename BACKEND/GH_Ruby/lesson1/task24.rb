6780d7837c20d8b36a985df61a1750209ade29fe
CeMuPaMuDa <bepyc9i@gmail.com>
2022-04-20 13:44:26 +0300

    task 23-24

---
+++ BACKEND/GH_Ruby/lesson1/task24.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+puts '24. Дано вещественное число R и массив вещественных чисел. Найти элемент массива, который наименее близок к данному числу.'
+puts "Вещественное число: #{r23 = rand(-50.0..50.0).round(2) }"
+puts "Исходный массив: #{arr_t23 = Array.new(15) { rand(-50.0..50.0).round(2) }}"
+
+r = arr_t23.each_with_object([]){|el, arr| arr << (el-r23).abs}
+result = arr_t23[r.index(r.max)]
+puts "Результат: #{r.index(r.max) + 1}-й элемент: #{result}"
